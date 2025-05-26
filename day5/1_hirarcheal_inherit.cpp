9a43be46feb0d9cbc3503f1d9fe70fc3737277a6
Abhisg-005 <abhiabhisheksg8114@gmail.com>
2025-05-26 23:14:56 +0530

    1_hirarcheal_inheritance updated

--- day5/1_hirarcheal_inherit.cpp
+++ day5/1_hirarcheal_inherit.cpp
@@ -21,11 +21,13 @@ class Student:public Person
     int roll_num;
 
     public:
-    Student(string name, int age, int roll_num ):Person(name,age),salary(salary){
+    Student(string name, int age, int roll_num ):Person(name,age), roll_num(roll_num){
 
     }
     void display_student(){
-        cout<<"Roll Num:"<<roll_num<<endl;
+        cout<<"Student Details:"<<endl;
+        display_person();
+        cout<<"Roll NO:"<<roll_num<<endl;
     }
 };
 
@@ -33,10 +35,10 @@ class Teacher:public Person{
     int salary;
 
     public:
-    Teacher(string name, int age, int salary),salary(salary){
+    Teacher(string name, int age, int salary): Person(name, age),salary(salary){
     }
     void display_teacher(){
-        cout<<"Teacher Details:"<<end;
+        cout<<"Teacher Details:"<<endl;
         display_person();
         cout<<"Salary"<<salary<<endl;
     }
@@ -44,8 +46,10 @@ class Teacher:public Person{
 
 int main()
 {
+    Student s1("vinith", 18,102);
+    s1.display_student();
     Teacher t1("pavan",23,20000);
     t1.display_teacher();
-    Student s1("vinith", 18,102);
-    s1.display_student;
+    
+    return 0;
 }
