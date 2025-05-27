0ce97f474fd8186a6dc965a3dd1110c6d3c86ed9
Abhisg-005 <abhiabhisheksg8114@gmail.com>
2025-05-27 21:45:54 +0530

    updated

--- day1_2/add.cpp
+++ day1_2/add.cpp
@@ -5,15 +5,19 @@ int main(){
     int num1 = 20;
     int num2 = 20;
 
-    int sum = num1+num2;
-    cout << "sum1:" << sum << endl;
+    int sum1 = num1+num2;
+    cout << "sum1:" << sum1 << endl;
 
     int num3 = 40;
     int num4 = 40;
+
+    int sum2 = num3 + num4;
+    cout<<"Sum2:"<<sum2<<endl;
     /*
     function:
     ========
     * it is a set of rules to solve the particular problem when it is called.
     */
     return 0;
+
 }
